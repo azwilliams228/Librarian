444971b320b454252f7e11d3e2094ba676b1397b
azwilliams228 <101468824+azwilliams228@users.noreply.github.com>
2022-03-30 07:06:00 -0400

    Update Librarian.py

--- Librarian.py
+++ Librarian.py
@@ -1,5 +1,21 @@
+from asyncio import set_event_loop
+import json
+from datetime import date
 
-with open('Library.txt', 'w') as f:
-    f.write('This is your dictionary.')
-    newWord = input ('What new word have you read?\n')
-    f.write(newWord)
+#with open('Library.json', 'a') as f:
+    #newWord = input ('What new word have you read?\n')
+    #today = date.today()
+    #dict = '{ "Word" : "newWord", "Date" : "today" }'
+    #person_dict = json.loads(dict)
+
+#with open('Library.json', 'r') as f:
+    #personalLibrary = json.load(f)
+
+today = date.today()
+word = "steven"
+words = { "Word" : word, "Date" : today }
+wordsjson = json.dumps(words, indent = 1, sort_keys = True, default = str) #Converts the string to a JSON string
+
+#words_dict = json.loads(words)
+
+print(wordsjson['Word'])
